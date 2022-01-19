70841111d262f9a0e082d2d6c92475abef9fdb5a
xiunhoxiu <thienly_xiu@hotmail.com>
2022-01-19 12:20:12 +0100

    built-in modules in Python - Exercise random

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import random
 
-members = ['John', 'Mary', 'Bob', 'Mosh']
-leader = random.choice(members)
-print(leader)
+
+class Dice:
+    def roll(self):
+        first = random.randint(1, 6)
+        second = random.randint(1, 6)
+        return first, second  # return in a tuple
+
+
+dice = Dice()
+print(dice.roll())
