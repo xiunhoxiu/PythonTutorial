d77525a07323f9bed2553a92a458bec986e338f0
xiunhoxiu <thienly_xiu@hotmail.com>
2022-01-20 13:17:03 +0100

    Directories - search using pathlib

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-import random
+from pathlib import Path
 
-
-class Dice:
-    def roll(self):
-        first = random.randint(1, 6)
-        second = random.randint(1, 6)
-        return first, second  # return in a tuple
-
-
-dice = Dice()
-print(dice.roll())
+path = Path()
+for file in path.glob('*'):
+    print(file)
