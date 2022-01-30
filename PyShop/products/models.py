e165517569a49ae409c447a393f769e8e1ef7957
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-01-30 14:30:24 +0100

    Migrations and db SQlite

--- PyShop/products/models.py
+++ PyShop/products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Product(models.Models):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     stock = models.IntegerField()
