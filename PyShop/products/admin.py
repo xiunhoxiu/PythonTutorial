abacd2237ed9a11a9f1370967b85f342caa2c5e0
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-01-31 10:54:32 +0100

    Customizating the Admin Panel
    Added new products and offers and made additional columns the Django Administration Panel

--- PyShop/products/admin.py
+++ PyShop/products/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Product
-from .models import Offer
+from .models import Product, Offer
 
 
-admin.site.register(Product)
-admin.site.register(Offer)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
