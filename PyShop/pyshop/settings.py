ebcb2484a060e7169f9df40bef58ff5257bd3a7e
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-01-31 22:04:44 +0100

    Added Final touches - End of tutorial
    Added NavBar, Made a reusable templates folder. Adjusted the Django path search, Added paddings. However, it can be fine-tuned even further.

--- PyShop/pyshop/settings.py
+++ PyShop/pyshop/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products.apps.ProductsConfig'
+    'products.apps.ProductsConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'pyshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
